eeef415d5d2a40c104683d33d3e22abcefa33461
Enno Richter <enno@nerdworks.de>
2022-08-23 15:10:43 +0200

    nmap

--- flake.nix
+++ flake.nix
@@ -1,8 +1,17 @@
 {
   description = "static-binaries";
 
-  outputs = { self, nixpkgs }: {
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-    defaultPackage.x86_64-linux = self.packages.x86_64-linux.hello;
-  };
+  inputs.nixpkgs.url = github:NixOS/nixpkgs/nixos-unstable;
+
+  outputs = { self, nixpkgs }:
+    let
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+      ];
+      forAllSystems = f: nixpkgs.lib.genAttrs supportedSystems (system: f system);
+    in
+    {
+      packages = forAllSystems (system: (import nixpkgs { inherit system; }));
+    };
 }

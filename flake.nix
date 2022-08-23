e24e15a1170ad1871bfa5fc81ea12d0b9c376961
Enno Richter <enno@nerdworks.de>
2022-08-23 15:56:30 +0200

    add pkgs, add release notes

--- flake.nix
+++ flake.nix
@@ -12,6 +12,26 @@
       forAllSystems = f: nixpkgs.lib.genAttrs supportedSystems (system: f system);
     in
     {
-      packages = forAllSystems (system: (import nixpkgs { inherit system; }));
+      defaultPackage = forAllSystems (system: (with import nixpkgs { inherit system; };  (
+        let
+          releaseNotes = writeText "release.txt" ''
+            - nmap v${nmap.version}
+            - python2 v${python2.version}
+            - tcpdump v${tcpdump.version}
+          '';
+        in
+        runCommand "static-binaries" { } ''
+          mkdir -p $out/bin
+          cp ${pkgsCross.musl32.pkgsStatic.nmap}/bin/nmap $out/bin/nmap-x86
+          cp ${pkgsCross.musl32.pkgsStatic.python2}/bin/python2.7 $out/bin/python2.7-x86
+          cp ${pkgsCross.musl32.pkgsStatic.tcpdump}/bin/tcpdump $out/bin/tcpdump-x86
+          cp ${pkgsCross.musl64.pkgsStatic.nmap}/bin/nmap $out/bin/nmap-x64
+          cp ${pkgsCross.musl64.pkgsStatic.python2}/bin/python2.7 $out/bin/python2.7-x64
+          cp ${pkgsCross.musl64.pkgsStatic.tcpdump}/bin/tcpdump $out/bin/tcpdump-x64
+
+          cp ${releaseNotes} $out/release.txt
+        ''
+      )
+      ));
     };
 }
